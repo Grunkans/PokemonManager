fdc506dce4dc8def252ae07bd8c15700d78da73a
Grunkans <143597576+Grunkans@users.noreply.github.com>
2023-12-18 10:18:40 +0100

    börjat hämta datan

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
-const url = 'https://api.adviceslip.com/advice'
-	fortuneButton.addEventListener('click' , async () => {
-		try{
-		const response = await fetch(url)
-		const data = await response.json()
+const searchButton = document.getElementById("buttonSearch");
 
-		console.log('Data from API:', data);
-		
-		// adviceContainer.innerText = data.slip.advice
-		// addToHistory(data.slip)
-								
-		}
-		catch(error) {
-			console.log('An error occurred! ' + error.message);
-		}
+const searchInput = document.getElementById("inputSearchField");
 
-		
-	
+ 
 
-		
-	})
+// Lägg till en klickhändelse till sökknappen
+
+searchButton.addEventListener("click", () => {
+
+    const searchTerm = searchInput.value.trim(); // Hämta sökterm och ta bort eventuell vitrymd
+
+ 
+
+    // Om söktermen inte är tom, gör ett API-anrop med söktermen
+
+    if (searchTerm !== "") {
+
+        const endpoint = `pokemon/${searchTerm}`; // Anpassa endpointen för Pokémon-sökningen
+
+ 
+
+        fetch(`https://pokeapi.co/api/v2/${endpoint}`)
+
+        .then((response) => {
+
+            if (!response.ok) {
+
+                throw new Error(`Anropet misslyckades med statuskod: ${response.status}`);
+
+            }
+
+            return response.json();
+
+        })
+
+        .then((data) => {
+
+            // Nu har du data från anropet till Pokéapi här
+
+            console.log(data);
+
+        })
+
+        .catch((error) => {
+
+            console.error("Det uppstod ett fel: ", error);
+
+        });
+
+    }
+
+});
